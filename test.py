3b94372bd7eb9dc5cff6e538ca056a45140088d2
GonzoMonk <sivlad004@yandex.ru>
2024-06-25 11:09:13 +0300

    Test1

--- test.py
+++ test.py
@@ -1,7 +1,29 @@
-print("Hello world")
+import requests
+import time
+import tokenFile  # type: ignore
 
-print("somebody once told me")
 
-print("One more time")
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = tokenFile.get_token()
+TEXT = 'Ура! Классный апдейт!'
+MAX_COUNTER = 100
 
-# End?
+offset = -2
+counter = 0
+chat_id: int
+
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
